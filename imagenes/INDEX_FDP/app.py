0acfc394419026aeef495d4cc110b97da1aff64a
adelacruzvilcarano-crypto <adelacruzvilcarano@gmail.com>
2026-06-10 08:52:42 -0500

    Add files via upload
    Actualiza esta parte del mis archivos con nuevas carpetas y nuevos archivos

---
+++ imagenes/INDEX_FDP/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("index_confecciones_gadiel.html")
+
+@app.route("/productos")
+def productos():
+    return render_template("productos.html")
+
+@app.route("/nosotros")
+def nosotros():
+    return render_template("nosotros.html")
+
+@app.route("/contactos")
+def contactos():
+    return render_template("contactos.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        usuario = request.form["usuario"]
+        password = request.form["password"]
+        with open("usuarios.txt", "a") as f:
+            f.write(f"{usuario},{password}\n")
+            return render_template("login.html", mensaje="Usuario registrado exitosamente")
+        # Aquí puedes agregar la lógica para autenticar al usuario
+        return redirect(url_for("inicio"))
+    return render_template("login.html")
+
+@app.route("/registro", methods=["GET", "POST"])
+def registro():
+    if request.method == "POST":
+        usuario = request.form["usuario"]
+        password = request.form["password"]
+        with open("usuarios.txt", "a") as f:
+            f.write(f"{usuario},{password}\n")
+            return render_template("registro.html", mensaje="Usuario registrado exitosamente")
+        # Aquí puedes agregar la lógica para registrar al usuario
+        return redirect(url_for("inicio"))
+    return render_template("registro.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
